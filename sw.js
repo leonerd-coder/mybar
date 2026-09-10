af72f11a8eb39a32edac8a2c23d9f2dde55b9dbc
leonerd-coder <leonerd@naver.com>
2026-09-10 20:25:00 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
-const CACHE_NAME = "mybar-v1";
+const CACHE_NAME = "mybar-v2";
+
 const FILES_TO_CACHE = [
   "./",
   "./index.html",
@@ -6,26 +7,31 @@ const FILES_TO_CACHE = [
   "./app.js",
   "./manifest.json"
 ];
+
 self.addEventListener("install", event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
       return cache.addAll(FILES_TO_CACHE);
     })
   );
+
   self.skipWaiting();
 });
+
 self.addEventListener("activate", event => {
   event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
+    caches.keys().then(keys => {
+      return Promise.all(
         keys
           .filter(key => key !== CACHE_NAME)
           .map(key => caches.delete(key))
-      )
-    )
+      );
+    })
   );
+
   self.clients.claim();
 });
+
 self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request).then(cached => {
